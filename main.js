68f484268d7c5f3a49a0e0a4bb9e53cbf6707a3c
santsossa <santiagosossaher@gmail.com>
2024-01-26 16:20:50 -0500

    se hace entrega

--- main.js
+++ main.js
@@ -1,24 +1,48 @@
+function calcularTotalCompra(cantidadProductos) {
+    let totalCompra = 0;
+
+    for (let i = 1; i <= cantidadProductos; i++) {
+        let precioProducto = parseFloat(prompt("Ingrese el precio del producto #" + i));
+        totalCompra += precioProducto;
+    }
+
+    return totalCompra.toFixed(2);
+}
+
+function concatenarTexto() {
+    let textoConcatenado = "";
+    let entradaTexto;
+
+    while (entradaTexto !== "ESC") {
+        entradaTexto = prompt("Ingrese un texto el cual se irá conectando con el anterior ('ESC' para salir):");
+        if (entradaTexto !== "ESC") {
+            textoConcatenado += entradaTexto + " ";
+            alert("Texto acumulado: " + textoConcatenado);
+        }
+    }
+}
+
+function repetirSaludo(vecesHola) {
+    for (let i = 0; i < vecesHola; i++) {
+        alert("Hola");
+    }
+}
+
 let cantidadProductos = parseInt(prompt("¿Cuántos productos compraste?"));
-let totalCompra = 0;
 
-for (let i = 1; i <= cantidadProductos; i++) {
-    let precioProducto = parseFloat(prompt("Ingrese el precio del producto #" + i));
-    totalCompra += precioProducto;
+if (!isNaN(cantidadProductos)) {
+    let totalCompra = calcularTotalCompra(cantidadProductos);
+    alert("Total de la compra: $" + totalCompra);
+} else {
+    alert("Por favor, ingresa un número válido para la cantidad de productos.");
 }
 
-alert("Total de la compra: $" + totalCompra.toFixed(2));
-
-let textoConcatenado = "";
-let entradaTexto;
-
-while (entradaTexto !== "ESC") {
-    entradaTexto = prompt("Ingrese un texto el cual se ira conectando con el anterior ('ESC' para salir):");
-    textoConcatenado += entradaTexto + " ";
-    alert("Texto acumulado: " + textoConcatenado);
-}
+concatenarTexto();
 
 let vecesHola = parseInt(prompt("¿Cuántas veces quieres que se repita el mensaje 'Hola'?"));
 
-for (let i = 0; i < vecesHola; i++) {
-    alert("Hola");
+if (!isNaN(vecesHola)) {
+    repetirSaludo(vecesHola);
+} else {
+    alert("Por favor, ingresa un número válido para la cantidad de repeticiones de 'Hola'.");
 }
